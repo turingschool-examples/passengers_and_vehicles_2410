ed0025d373c7c1ddec66095499fb679b3f82b664
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-23 13:39:59 -0400

    Update Park file/spec to include tests for all of Iteration 3

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -10,16 +10,35 @@ RSpec.describe Park do
             park1 = Park.new('Shenandoah', 20)
             
             expect(park1).to be_a Park
+        end
+        
+        it 'has a name and lists admission price per adult' do
+            park1 = Park.new('Shenandoah', 20)
 
             expect(park1.name).to eq('Shenandoah')
             expect(park1.admission).to eq(20)
-
-            expect(park1.vehicles).to eq([])
-            expect(park1.passengers).to eq([])
-            expect(park1.admission_collected).to eq(0)
+            
         end
+  
+        it 'can list all vehicles entered' do
+            park1 = Park.new('Shenandoah', 20)
+        
+            expect(park1.vehicles).to eq([])
+        end
+
+        it 'can list all passengers admitted' do
+            park1 = Park.new('Shenandoah', 20)
+
+            expect(park1.passengers).to eq([])
+        end
+
     end
 
-    describe ''
+    it 'can generate total revenue collected from admission charged' do
+        park1 = Park.new('Shenandoah', 20)
+        
+        expect(park1.admission_collected).to eq(0)
+    end
+
 end
 
