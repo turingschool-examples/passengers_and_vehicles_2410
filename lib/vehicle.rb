a33d8deff8dc4c0119ae1006c6016e1c2932d70d
Dustin Peukert <dpeukert04@gmail.com>
2024-10-23 12:20:55 -0400

    write #speeding? and #speed

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,10 +1,19 @@
 class Vehicle
   attr_reader :year, :make, :model, :passengers
-  
+
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
     @passengers = []
+    @speeding = false
+  end
+
+  def speeding?
+    @speeding
+  end
+
+  def speed
+    @speeding = true
   end
 end
