6bfdabbda1128ef2ba222ca39b5165c586bd6f9d
Patrick Shim <patrick.shim91@yahoo.com>
2024-10-23 12:39:45 -0400

    added initialize method tests for Park class

--- lib/park.rb
+++ lib/park.rb
@@ -1,8 +1,9 @@
 class Park
-    attr_reader :name, :price
+    attr_reader :name, :price, :vehicles
 
     def initialize(name, price)
         @name = name
         @price = price
+        @vehicles = []
     end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -7,8 +7,31 @@ RSpec.configure do |config|
 end
 
 RSpec.describe Park do 
+  before(:each) do
+    @park1 = Park.new("Yosemite", 35)
+    @park2 = Park.new("Yellowstone", 30)
+    @park3 = Park.new("Grand Canyon", 40)
+  end
+
   describe "#initialization" do 
     it "exists" do 
+      # park 1
+      expect(@park1).to be_a(Park)
+      expect(@park1.name).to eq("Yosemite")
+      expect(@park1.price).to eq(35)
+      expect(@park1.vehicles).to eq([])
+
+      # park 2
+      expect(@park2).to be_a(Park)
+      expect(@park2.name).to eq("Yellowstone")
+      expect(@park2.price).to eq(30)
+      expect(@park2.vehicles).to eq([])
+
+      #park 3
+      expect(@park3).to be_a(Park)
+      expect(@park3.name).to eq("Grand Canyon")
+      expect(@park3.price).to eq(40)
+      expect(@park3.vehicles).to eq([])
     end
   end
 end
