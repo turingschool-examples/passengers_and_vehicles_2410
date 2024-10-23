0aae7755659f5fc6f5e6d6227a00ec0942c9cfad
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-23 12:25:56 -0400

    Update Vehicle spec to pass initialization test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,7 +8,17 @@ RSpec.describe Vehicle do
     describe "#initialization" do
         it "exists" do 
             vehicle = Vehicle.new("2001", "Honda", "Civic")    
+            
             expect(vehicle).to be_a Vehicle 
         end
+
+        it 'assigns year/make/model to new vehicle' do
+            vehicle = Vehicle.new("2001", "Honda", "Civic")    
+           
+            expect(vehicle.year).to eq("2001")
+            expect(vehicle.make).to eq("Honda")
+            expect(vehicle.model).to eq("Civic")
+        end
     end
+
 end
