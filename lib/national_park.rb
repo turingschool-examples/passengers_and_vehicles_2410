0e473ad71dc6534f26f668eb8507f2762f9de17a
Dustin Peukert <dpeukert04@gmail.com>
2024-10-23 12:26:22 -0400

    create and initialize NationalPark class

---
+++ lib/national_park.rb
@@ -0,0 +1,15 @@
+class NationalPark
+  attr_reader :name,
+              :admission,
+              :entered_passengers,
+              :entered_vehicles,
+              :revenue
+
+  def initialize(park_info)
+    @name = park_info[:name]
+    @admission = park_info[:admission]
+    @entered_passengers = []
+    @entered_vehicles = []
+    @revenue = 0
+  end
+end
