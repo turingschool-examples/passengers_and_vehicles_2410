dae34e5d60f987c0e48834780052fa702f291e20
Kevin Newland <kevin.newland@yahoo.com>
2024-10-23 12:51:04 -0500

    added passenger_spec

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,4 +1,7 @@
 require './lib/passenger'
+require 'pry'
+require 'rspec'
+
 
 RSpec.configure do |config|
   config.formatter = :documentation
@@ -7,8 +10,36 @@ end
 RSpec.describe Passenger do 
   describe "#initialization" do 
     it "exists" do 
-      charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
-      expect(charlie).to be_a Passenger
+      @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+      @taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+      expect(@charlie).to be_a Passenger
+      expect(@taylor).to be_a Passenger 
+    end
+
+    it "passenger has attributes" do 
+      @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+      @taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+    
+      expect(@charlie.name).to be_a(String)
+      expect(@charlie.age).to be_a(Integer)
+      expect(@taylor.name).to be_a(String)
+      expect(@taylor.age).to be_a(Integer)
+    end
+
+    it "checks to see if adult?" do
+      @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+      @taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+      expect(@charlie.adult?).to be(true)
+      expect(@taylor.adult?).to be(false)
+    end
+
+    it "checks to see if charlie can drive" do
+      @charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+      @taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+      expect(@charlie.driver?).to be(true)
     end
   end
 end
