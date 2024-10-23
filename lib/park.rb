358599f66af47243913a07b5c83028e456e41505
elysableu <elysabward@gmail.com>
2024-10-23 10:59:13 -0600

    Add admit_vehicle method

--- lib/park.rb
+++ lib/park.rb
@@ -1,9 +1,18 @@
 class Park
-  attr_reader :name, :admission
+  attr_reader :name, 
+              :admission, 
+              :admitted_vehicles, 
+              :admitted_passengers
 
   def initialize(park_data)
     @name = park_data["name"]
     @admission = park_data["admission"]
+    @admitted_vehicles = []
+    @admitted_passengers = []
   end
 
+  def admit_vehicle(vehicle)
+    @admitted_vehicles << vehicle
+  end
+  
 end
