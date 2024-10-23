bf81fa740e10648c02587f5499d5380e79266ac7
Dustin Peukert <dpeukert04@gmail.com>
2024-10-23 12:18:20 -0400

    write #driver? and #drive

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -4,9 +4,18 @@ class Passenger
   def initialize(person_info)
     @name = person_info["name"]
     @age = person_info["age"]
+    @driver = false
   end
 
   def adult?
     @age >= 18
+  end
+
+  def driver?
+    @driver
+  end
+
+  def drive
+    @driver = true
   end
 end
