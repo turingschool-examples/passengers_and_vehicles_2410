fe1da794eba6e10372e1aee70aef5c5be83488f3
Dustin Peukert <dpeukert04@gmail.com>
2024-10-23 12:16:35 -0400

    write #adult?

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,6 +1,12 @@
 class Passenger
+  attr_reader :name, :age
+
   def initialize(person_info)
     @name = person_info["name"]
     @age = person_info["age"]
+  end
+
+  def adult?
+    @age >= 18
   end
 end
