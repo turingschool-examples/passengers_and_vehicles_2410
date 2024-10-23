ddc3a2498f622cc40e8bc61911f70d0979f1cd8d
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-23 13:08:39 -0400

    Complete Iteration2 with vehicle passengers

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,12 @@
 class Vehicle
-    attr_reader :year, :make, :model
+    attr_reader :year, :make, :model, :speeding, :passengers
 
     def initialize(year, make, model)
         @year = year
         @make = make
         @model = model
         @speeding = false
+        @passengers = []
     end
 
     def speeding? 
@@ -14,6 +15,17 @@ class Vehicle
 
     def speed
         @speeding = true
+    end
+
+    def add_passenger(passenger)
+        @passengers << passenger
+    end
+
+    def num_adults
+        num_adults = 0
+        if @passengers.adult?
+            num_adults += 1
+        end
     end
 
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/vehicle'
+require_relative '../lib/passenger'
 
 RSpec.configure do |config|
     config.formatter = :documentation
@@ -32,8 +33,47 @@ RSpec.describe Vehicle do
         it 'reassigns #speeding to true' do
             vehicle = Vehicle.new("2001", "Honda", "Civic")    
             vehicle.speed
+            
             expect(vehicle.speeding?).to eq(true)
         end
+    end
+
+    describe '#passengers' do
+        it 'is an empty array at initialization' do
+            vehicle = Vehicle.new("2001", "Honda", "Civic")    
+            expect(vehicle.passengers).to eq([])
+        end
+
+        it 'can assign passengers to vehicle' do
+            vehicle = Vehicle.new("2001", "Honda", "Civic")    
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+            
+            vehicle.add_passenger(charlie)
+            expect(vehicle.passengers.count).to eq(1)
+
+            vehicle.add_passenger(jude)
+            expect(vehicle.passengers.count).to eq(2)
+
+            vehicle.add_passenger(taylor)
+            expect(vehicle.passengers.count).to eq(3)
+        end
+
+        it 'can count the number of adult passengers' do
+            vehicle = Vehicle.new("2001", "Honda", "Civic")    
+           
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+            
+            vehicle.add_passenger(charlie)
+            vehicle.add_passenger(jude)
+            vehicle.add_passenger(taylor)
+            # require 'pry'; binding.pry
+            # expect(vehicle.num_adults).to eq(2)
+        end
+
     end
 
 end
