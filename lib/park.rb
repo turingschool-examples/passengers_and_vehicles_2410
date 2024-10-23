d112bdc160cc66797e83bb0c41c10b9a9c168a66
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-23 13:32:21 -0400

    Update Park initialization spec/lib

--- lib/park.rb
+++ lib/park.rb
@@ -1,9 +1,17 @@
 class Park 
-    attr_accessor :name, :admission
+    attr_accessor :name, :admission, :vehicles, :passengers, :admission_collected
 
     def initialize(name, admission)
         @name = name
         @admission = admission
+        @vehicles = []
+        @passengers = []
+        @admission_collected = 0
     end
 
+    def admit_vehicle
+    end
+
+    def admit_passenger
+    end     
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -10,7 +10,16 @@ RSpec.describe Park do
             park1 = Park.new('Shenandoah', 20)
             
             expect(park1).to be_a Park
+
+            expect(park1.name).to eq('Shenandoah')
+            expect(park1.admission).to eq(20)
+
+            expect(park1.vehicles).to eq([])
+            expect(park1.passengers).to eq([])
+            expect(park1.admission_collected).to eq(0)
         end
     end
+
+    describe ''
 end
 
