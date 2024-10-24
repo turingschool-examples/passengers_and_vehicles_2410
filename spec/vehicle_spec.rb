b0325d1be36ca1c117cbdaf885943855b0deb3de
memcmahon <mcmahon.meganelizabeth@gmail.com>
2024-10-24 09:22:56 -0600

    adds vehicle methods

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -20,4 +20,42 @@ RSpec.describe Vehicle do
       expect(vehicle.model).to eq "Civic"
     end
   end
+
+  it "can speed" do 
+      vehicle = Vehicle.new("2001", "Honda", "Civic")  
+
+      expect(vehicle.speeding?).to be false 
+
+      vehicle.speed 
+
+      expect(vehicle.speeding?).to be true 
+  end
+
+  it "can add passengers" do 
+      vehicle = Vehicle.new("2001", "Honda", "Civic")  
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+      jude = Passenger.new({"name" => "Jude", "age" => 20})    
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+      expect(vehicle.passengers).to eq([])
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(vehicle.passengers).to eq([charlie, jude, taylor])
+  end
+
+  it "can give count of adults" do 
+    vehicle = Vehicle.new("2001", "Honda", "Civic")  
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    jude = Passenger.new({"name" => "Jude", "age" => 20})    
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+    vehicle.add_passenger(charlie)
+    vehicle.add_passenger(jude)
+    vehicle.add_passenger(taylor)
+
+    expect(vehicle.num_adults).to eq 2
+  end
 end
